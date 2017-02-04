50e470746ff2b1552057f1aae131610527269a86
gilbertdclp <gilbert.dclp@gmail.com>
2017-02-03 17:56:40 -0800

    Update individualized_company_report_creation.py

--- individualized_company_report_creation.py
+++ individualized_company_report_creation.py
@@ -3,7 +3,7 @@
 import csv
 
 # reading file
-csv_filename = "state_ui_ee_form.csv"
+csv_filename = "ACH_Data_mon_1.csv"
 csv_file = open(csv_filename)
 csv_reader = csv.reader(csv_file)
 csv_rows = []
@@ -18,7 +18,7 @@ csv_contents = csv_file.read()
 # writing files for each column
 # print csv_rows[0]
 index = 1
-csv_filename_new = csv_rows[index][1] + ' NJ 927 Q4_state_ui_ee_form.csv'
+csv_filename_new = csv_rows[index][1] + ' ACH_Transactions_mon_1.csv'
 csv_file_new = open(csv_filename_new, "w+")
 csv_writer = csv.writer(csv_file_new)
 csv_writer.writerow(csv_rows[0])
@@ -28,7 +28,7 @@ for row in csv_rows[2:]:
 	if row[1] == csv_rows[index][1]:
 		csv_writer.writerow(row)
 	else:
-		csv_filename_new = row[1] + ' NJ 927 Q4_state_ui_ee_form.csv'
+		csv_filename_new = row[1] + ' ACH_Transactions_mon_1.csv'
 		csv_file_new = open(csv_filename_new, "w+")
 		csv_writer = csv.writer(csv_file_new)
 		csv_writer.writerow(csv_rows[0])
